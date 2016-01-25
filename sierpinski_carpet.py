c2e508b6e16dd5ee0ff8c892157d9279ecf15e2f
csgray <csgray@alaska.edu>
2016-01-25 11:50:37 -0900

    Finish sierpinsky_carpet.py to four iterations

--- sierpinski_carpet.py
+++ sierpinski_carpet.py
@@ -5,31 +5,56 @@ window.bgcolor("white")
 alpha = turtle.Turtle()
 alpha.shape(None)
 alpha.color("blue")
-alpha.speed(10)
+alpha.speed(0)
+alpha.penup()
+alpha.goto(-200,200)
+alpha.pendown()
 
-def draw_square(turtle):
+def draw_square(turtle, size):
 	turtle.fill(True)
 	for i in range(1,5):
-		turtle.forward(10)
+		turtle.forward(size)
 		turtle.right(90)
 	turtle.fill(False)
 
-def draw_fractal(turtle):
+def draw_fractal(turtle, size):
+	"""Draws a larger square with the middle square missing."""
 	for i in range(1,5):
 		for i in range(1,3):
-			draw_square(turtle)
-			turtle.forward(10)
-		turtle.forward(10)
+			draw_square(turtle, size)
+			turtle.forward(size)
+		turtle.forward(size)
 		turtle.right(90)
 
-def draw_sierpinski(turtle):
-	for i in range(1,2):
-		draw_fractal(turtle)
+def draw_fractal2(turtle, size):
+	for i in range(1,5):
+		for i in range(1,3):
+			draw_fractal(turtle, size)
+			turtle.forward(size * 3)
+		turtle.forward(size * 3)
 		turtle.right(90)
-		turtle.forward(30)
-	turtle.hideturtle()
 
-#draw_square(alpha)
-draw_fractal(alpha)
-#draw_sierpinski(alpha)
+def draw_fractal3(turtle, size):
+	for i in range(1,5):
+		for i in range(1,3):
+			draw_fractal2(turtle, size)
+			turtle.forward(size * 9)
+		turtle.forward(size * 9)
+		turtle.right(90)
+
+def draw_fractal4(turtle, size):
+	for i in range(1,5):
+		for i in range(1,3):
+			draw_fractal3(turtle, size)
+			turtle.forward(size * 27)
+		turtle.forward(size * 27)
+		turtle.right(90)
+
+draw_square(alpha, 2)
+#draw_fractal(alpha, 2)
+#draw_fractal2(alpha, 1)
+#draw_fractal3(alpha, 2)
+#draw_fractal4(alpha, 2)
+
+alpha.hideturtle()
 window.exitonclick()
